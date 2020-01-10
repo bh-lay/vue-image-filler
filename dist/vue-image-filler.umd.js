0e563cee0ff0c0b532e074fe50b73441d70d33dd
bh-lay <mail@bh-lay.com>
2020-01-11 01:26:58 +0800

    整理 demo

--- dist/vue-image-filler.umd.js
+++ dist/vue-image-filler.umd.js
@@ -16,66 +16,84 @@
 	    return css;
 	}
 
+	// 清除页面选择，组织浏览器默认事件防止页面抖动
 	function preventDefault(event) {
 		window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
 		event.preventDefault && event.preventDefault();
 		event.stopPropagation && event.stopPropagation();
 	}
-	function getParam(clientX, clientY, startX, startY) {
+	// 回调触发器
+	function stepCall(callback, event, startX, startY) {
+		if (!callback) {
+			return;
+		}
+		var clientX = event.clientX;
+		var clientY = event.clientY;
 		var xOffset = clientX - startX;
 		var yOffset = clientY - startY;
 
-		return {
+		callback({
 			clientX: clientX,
 			clientY: clientY,
 			xOffset: xOffset,
 			yOffset: yOffset
-		};
+		});
 	}
-	function dragHandle(event, _ref) {
-		var move = _ref.move,
-		    end = _ref.end;
 
-		var isTouch = event.type === 'touchstart';
-		var startX = event.clientX;
-		var startY = event.clientY;
+	// 触摸屏逻辑
+	function touch(event, move, end) {
+		var startX = event.touches[0].clientX;
+		var startY = event.touches[0].clientY;
+		var lastTouch = void 0;
 
-		preventDefault(event);
-		if (isTouch) {
-			startX = event.touches[0].clientX;
-			startY = event.touches[0].clientY;
-			event.target.addEventListener('touchmove', touchmove, false);
-			event.target.addEventListener('touchend', touchend, false);
-			event.target.addEventListener('touchcancel', touchend, false);
-		} else {
-			document.addEventListener('mousemove', mousemove, false);
-			document.addEventListener('mouseup', mouseup, false);
-		}
-		var lastClientX = void 0;
-		var lastClientY = void 0;
 		function touchmove(e) {
 			preventDefault(e);
-			lastClientX = e.touches[0].clientX;
-			lastClientY = e.touches[0].clientY;
-			move && move(getParam(lastClientX, lastClientY, startX, startY));
+			lastTouch = e.touches[0];
+			stepCall(move, lastTouch, startX, startY);
 		}
+
 		function touchend(e) {
 			event.target.removeEventListener('touchmove', touchmove, false);
 			event.target.removeEventListener('touchend', touchend, false);
 			event.target.removeEventListener('touchcancel', touchend, false);
-			end && end(getParam(lastClientX, lastClientY, startX, startY));
+			stepCall(end, lastTouch, startX, startY);
 		}
+
+		event.target.addEventListener('touchmove', touchmove, false);
+		event.target.addEventListener('touchend', touchend, false);
+		event.target.addEventListener('touchcancel', touchend, false);
+	}
+	// 鼠标逻辑
+	function mouse(event, move, end) {
+		var startX = event.clientX;
+		var startY = event.clientY;
 
 		function mousemove(e) {
 			preventDefault(e);
 			window.getSelection ? window.getSelection().removeAllRanges() : document.selection.empty();
-			move && move(getParam(e.clientX, e.clientY, startX, startY));
+			stepCall(move, e, startX, startY);
 		}
+
 		function mouseup(e) {
 			document.removeEventListener('mousemove', mousemove, false);
 			document.removeEventListener('mouseup', mouseup, false);
+			stepCall(end, e, startX, startY);
+		}
 
-			end && end(getParam(e.clientX, e.clientY, startX, startY));
+		document.addEventListener('mousemove', mousemove, false);
+		document.addEventListener('mouseup', mouseup, false);
+	}
+
+	// 主逻辑
+	function dragHandle(event, _ref) {
+		var move = _ref.move,
+		    end = _ref.end;
+
+		preventDefault(event);
+		if (event.type === 'touchstart') {
+			touch(event, move, end);
+		} else {
+			mouse(event, move, end);
 		}
 	}
 
@@ -234,8 +252,8 @@
 				var fileBlobURL = window.URL.createObjectURL(new Blob([file]));
 				var image = new Image();
 				image.onload = function () {
-					_this.size.imageWidth = image.naturalWidth;
-					_this.size.imageHeight = image.naturalHeight;
+					_this.size.imageWidth = image.width || image.naturalWidth;
+					_this.size.imageHeight = image.height || image.naturalHeight;
 					_this.size.canvasWidth = _this.$refs.outer.clientWidth;
 
 					var _getBoxSizePlaceIntoA = getBoxSizePlaceIntoAnotherBox(_this.cropWidth, _this.cropHeight, _this.size.canvasWidth, _this.size.canvasHeight),
